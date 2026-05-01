efd9b8f76e90e1e414d458048077c410a1eeade0
nidhiisharma07 <148667452+nidhiisharma07@users.noreply.github.com>
2026-05-01 23:45:39 +0530

    Update api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import NProgress from "nprogress";
 
 const api = axios.create({
-  baseURL: "import.meta.env.VITE_API_URL"
+  baseURL: import.meta.env.VITE_API_URL, // ❗ NO quotes
 });
 
 // 🔥 ADD TOKEN AUTOMATICALLY
