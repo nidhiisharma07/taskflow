7196728d48b422b5bc7657dbcde95213d47e5251
nidhiisharma07 <148667452+nidhiisharma07@users.noreply.github.com>
2026-05-01 19:33:01 +0530

    Delete tailwind.config.js

--- tailwind.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-export default {
-  darkMode: "class", // ✅ THIS LINE FIXES YOUR ISSUE
-  content: ["./index.html", "./src/**/*.{js,jsx}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-};
