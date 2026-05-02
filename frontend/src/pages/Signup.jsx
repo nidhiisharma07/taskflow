774a7fc642e0a71d64bca9e7c13482b6b81218d6
nidhiisharma07 <148667452+nidhiisharma07@users.noreply.github.com>
2026-05-02 11:30:09 +0530

    Update Signup.jsx

--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import api from "../services/api";
+console.log("API:", import.meta.env.VITE_API_URL);
 
 const Signup = () => {
   const navigate = useNavigate();
